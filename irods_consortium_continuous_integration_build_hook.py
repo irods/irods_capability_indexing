c60c08a52619fc25eb6e28eef2b742f21dc2db45
d-w-moore <dmoore@renci.org>
2021-12-13 20:10:33 -0500

    [irods/irods#6011] reverse dev and runtime installation order

--- irods_consortium_continuous_integration_build_hook.py
+++ irods_consortium_continuous_integration_build_hook.py
@@ -57,14 +57,18 @@ def install_os_specific_dependencies():
     except KeyError:
         irods_python_ci_utilities.raise_not_implemented_for_distribution()
 
+
 def install_irods_dev_and_runtime_packages(irods_packages_root_directory):
+
     irods_packages_directory = irods_python_ci_utilities.append_os_specific_directory(irods_packages_root_directory)
-    dev_package_basename = filter(lambda x:'irods-dev' in x, os.listdir(irods_packages_directory))[0]
-    dev_package = os.path.join(irods_packages_directory, dev_package_basename)
-    irods_python_ci_utilities.install_os_packages_from_files([dev_package])
+# starting in iRODS 4.2.11 , irods-dev has a dependency on irods-runtime
     runtime_package_basename = filter(lambda x:'irods-runtime' in x, os.listdir(irods_packages_directory))[0]
     runtime_package = os.path.join(irods_packages_directory, runtime_package_basename)
     irods_python_ci_utilities.install_os_packages_from_files([runtime_package])
+
+    dev_package_basename = filter(lambda x:'irods-dev' in x, os.listdir(irods_packages_directory))[0]
+    dev_package = os.path.join(irods_packages_directory, dev_package_basename)
+    irods_python_ci_utilities.install_os_packages_from_files([dev_package])
 
 def copy_output_packages(build_directory, output_root_directory):
     irods_python_ci_utilities.gather_files_satisfying_predicate(
